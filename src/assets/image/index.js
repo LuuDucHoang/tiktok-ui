04a0180b8d36097ffa945d4a0e394da51ba58c19
LuuDucHoang <livetolove2736@gmail.com>
2023-03-22 14:49:18 +0700

    Tự chỉnh sủa UI

--- src/assets/image/index.js
+++ src/assets/image/index.js
@@ -1,4 +1,5 @@
 const images = {
     logo: require('~/assets/image/logo.svg').default,
+    inconEffect: require('~/assets/image/icon.svg').default,
 };
 export default images;
--- src/components/Layout/Components/Header/index.js
+++ src/components/Layout/Components/Header/index.js
@@ -2,7 +2,13 @@ import style from './Header.module.scss';
 import classNames from 'classnames/bind';
 import images from '~/assets/image';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import {
+    faCircleXmark,
+    faSpinner,
+    faMagnifyingGlass,
+    faPlus,
+    faEllipsisVertical,
+} from '@fortawesome/free-solid-svg-icons';
 const cx = classNames.bind(style);
 function Header() {
     return (
@@ -19,7 +25,19 @@ function Header() {
                         <FontAwesomeIcon icon={faMagnifyingGlass}></FontAwesomeIcon>
                     </button>
                 </div>
-                <div className={cx('action')}></div>
+                <div className={cx('action')}>
+                    <button className={cx('upload-btn')}>
+                        <div>
+                            <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+                        </div>
+                        Upload
+                    </button>
+                    <button className={cx('login-btn')}>Log in</button>
+                    <img className={cx('effect-icon')} src={images.inconEffect} alt={'Anh effect'}></img>
+                    <button className={cx('setting-btn')}>
+                        <FontAwesomeIcon icon={faEllipsisVertical}></FontAwesomeIcon>
+                    </button>
+                </div>
             </div>
         </header>
     );
